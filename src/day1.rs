9827641c959a6868f7000163db0efa2bd42fad25
Roy Jacobs <roy.jacobs@gmail.com>
2022-12-01 16:01:30 +0100

    Day 1

---
+++ src/day1.rs
@@ -0,0 +1,30 @@
+use std::collections::HashMap;
+use itertools::Itertools;
+
+pub fn day1() -> anyhow::Result<()> {
+    let lines = include_str!("day1.txt").lines();
+
+    let mut calories: HashMap<usize, usize> = HashMap::new();
+    let mut elf: usize = 1;
+    for line in lines {
+        if line.is_empty() {
+            elf += 1;
+        } else {
+            let e = calories.entry(elf).or_insert(0);
+            *e += usize::from_str_radix(line, 10)?;
+        }
+    }
+
+    let calories = calories.into_iter()
+        .sorted_by(|a, b| a.1.cmp(&b.1))
+        .map(|e| e.1)
+        .rev()
+        .collect_vec();
+
+    let max_calories_single_elf: usize = *calories.first().unwrap();
+    println!("{:?}", max_calories_single_elf);
+    let max_calories: usize = calories[0..3].into_iter().sum();
+    println!("{:?}", max_calories);
+
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use crate::day1::day1;
+
+mod day1;
+
+fn main() -> anyhow::Result<()> {
+    day1()?;
+    Ok(())
+}
